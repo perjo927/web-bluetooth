2ae2643d6da6c55fb793676b0d8dce6e423e8eaf
perjo927 <perjo927@student.liu.se>
2017-08-13 18:54:57 +0200

    additional features

--- index.js
+++ index.js
@@ -2,13 +2,34 @@ const button = document.querySelector('#the-button');
 
 button.addEventListener('click', function() {
 	navigator.bluetooth.requestDevice({
-		filters: [{
-			services: ['battery_service']
-		}]
-	}).then(device => {
-		console.log('Got device:', device.name);
+        filters: [{
+            services: ['battery_service']
+        }]
+    }).then(device => {
+        console.log('Got device:', device.name);
+        alert(device.name);        
         console.log('id:', device.id);
-	}).catch(e => {
-        console.log(e);
+        return device.gatt.connect(); // Chromium 49 and below use `connectGATT()` but from Chromium 50 it will use gatt.connect();
+    })
+    .then(server => {
+        console.log('Getting Battery Service…');
+        alert('Getting Battery Service…');
+        return server.getPrimaryService('battery_service');
+    })
+    .then(service => {
+        console.log('Getting Battery Characteristic…');
+        return service.getCharacteristic('battery_level');
+    })
+    .then(characteristic => {
+        console.log('Reading battery level…');
+        return characteristic.readValue();
+    })
+    .then(value => {
+        value = value.buffer ? value : new DataView(value);
+        console.log('Battery percentage:', value.getUint8(0));
+    })
+    .catch(exception => {
+        console.log(exception);
+        alert(exception);
     });
 });
